2680eaf952d1b2a4529c6641fc97e9263408c62c
pushTest <140990768+sp1daTomm@users.noreply.github.com>
2024-02-14 21:12:24 +0800

    upperTolowerCase

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
   {
     path: '/pullInfo',
     name: 'PullInfo',
-    component: () => import('../views/PullInfo.vue'),
+    component: () => import('../views/pullInfo.vue'),
   },
 ];
 
