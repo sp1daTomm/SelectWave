138191d46a480fb8f183ea8e6ee5c380288045f8
Pei Ying <eee317@gmail.com>
2024-02-15 22:58:02 +0800

    router加入巢狀

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,3 @@
 <template>
-  <Navbar />
   <div style="height:200vh" class="max-w-screen-lg mx-auto">eujowo</div>
-  <TopGo />
-  <ComponentFooter />
 </template>
-
-<script setup>
-import ComponentFooter from '@/components/ComponentFooter.vue';
-import Navbar from '@/components/NavbarEl.vue';
-import TopGo from '@/components/TopGoEl.vue';
-</script>
---
+++ src/views/WebLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <Navbar />
+  <RouterView />
+  <TopGo />
+  <ComponentFooter />
+</template>
+<script setup>
+import ComponentFooter from '@/components/ComponentFooter.vue';
+import Navbar from '@/components/NavbarEl.vue';
+import TopGo from '@/components/TopGoEl.vue';
+</script>
